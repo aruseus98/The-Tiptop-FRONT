a6160ac8a66df5387197b14fa6f2ffc805456fe2
aruseus98 <aruseus98@gmail.com>
2023-11-30 10:30:56 +0100

    Correction AuthGuard, tableau retiré et take + renvoi observable

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
-import { Observable, map, take, catchError, switchMap } from 'rxjs';
+import { Observable, map, take, catchError, switchMap, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,9 @@ export class authGuard implements CanActivate {
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.isLoggedIn().pipe(
       switchMap((loggedIn: boolean) => {
-        take(1) // Prend la valeur actuelle et complète l'Observable
         if (!loggedIn) {
           this.router.navigate(['auth/login']);
-          return [false]; // retourner un tableau ici pour que cela corresponde à la signature Observable<boolean>
+          return of(false); // Renvoie un nouvel Observable indiquant que l'authentification a échoué
         }
         return this.authService.getUserInfoFromCookie().pipe(
           map(userInfo => {
@@ -28,8 +27,9 @@ export class authGuard implements CanActivate {
           }),
           catchError(error => {
             // Si une erreur se produit (par exemple, un token invalide), redirigez l'utilisateur vers la page de connexion
+            console.error('Une erreur est survenue lors de la vérification de l\'authentification: ', error);
             this.router.navigate(['auth/login']);
-            return [false];
+            return of(false); // Renvoie un nouvel Observable indiquant que l'authentification a échoué
           })
         );
       })
